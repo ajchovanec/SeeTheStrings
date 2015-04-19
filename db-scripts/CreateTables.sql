7718b73b6f1ccd4514b43dc20b8a3c5008102a62
Alex Chovanec <alex.chovanec@gmail.com>
2015-04-19 03:41:19 -0700

    split db creation and importing into separate scripts

---
+++ db-scripts/CreateTables.sql
@@ -0,0 +1,105 @@
+CREATE TABLE Categories (
+    CatCode TEXT,
+    CatName TEXT,
+    CatOrder TEXT,
+    Industry TEXT,
+    Sector TEXT,
+    SectorLong TEXT);
+
+CREATE TABLE Candidates (
+    Cycle TEXT,
+    FECCandID TEXT,
+    CID TEXT,
+    FirstLastP TEXT,
+    Party TEXT,
+    DistIDRunFor TEXT,
+    DistIDCurr TEXT,
+    CurrCand TEXT,
+    CycleCand TEXT,
+    CRPICO TEXT,
+    RecipCode TEXT,
+    NoPacs TEXT
+);
+
+CREATE TABLE Committees (
+    Cycle TEXT,
+    CmteID TEXT,
+    PACShort TEXT,
+    Affiliate TEXT,
+    UltOrg TEXT,
+    RecipID TEXT,
+    RecipCode TEXT,
+    FECCandID TEXT,
+    Party TEXT,
+    PrimCode TEXT,
+    Source TEXT,
+    Sensitive TEXT,
+    IsForeign INTEGER,
+    Active INTEGER
+);
+ 
+CREATE TABLE PACsToCandidates (
+    Cycle TEXT,
+    FECRecNo TEXT,
+    PACID TEXT,
+    CID TEXT,
+    Amount REAL,
+    Date TEXT,
+    RealCode TEXT,
+    Type TEXT,
+    DirectOrIndirect TEXT,
+    FECCandID TEXT
+);
+
+CREATE TABLE PACsToPACs (
+    Cycle TEXT,
+    FECRecNo TEXT,
+    FilerID TEXT,
+    DonorCmte TEXT,
+    ContribLendTrans TEXT,
+    City TEXT,
+    State TEXT,
+    Zip TEXT,
+    FECOccEmp TEXT,
+    PrimCode TEXT,
+    Date TEXT,
+    Amount REAL,
+    RecipID TEXT,
+    Party TEXT,
+    OtherID TEXT,
+    RecipCode TEXT,
+    RecipPrimCode TEXT,
+    Amend TEXT,
+    Report TEXT,
+    PrimaryOrGeneral TEXT,
+    Microflim TEXT,
+    Type TEXT,
+    RealCode TEXT,
+    Source TEXT
+);
+
+CREATE TABLE IndivsToCandidates (
+    Cycle TEXT,
+    FECTransID TEXT,
+    ContribID TEXT,
+    Contrib TEXT,
+    RecipID TEXT,
+    OrgName TEXT,
+    UltOrg TEXT,
+    RealCode TEXT,
+    Date TEXT,
+    Amount INTEGER,
+    Street TEXT,
+    City TEXT,
+    State TEXT,
+    Zip TEXT,
+    RecipCode TEXT,
+    Type TEXT,
+    CmteID TEXT,
+    OtherID TEXT,
+    Gender TEXT,
+    Microfilm TEXT,
+    Occupation TEXT,
+    Employer TEXT,
+    Source TEXT
+);
---
+++ db-scripts/sqlite-scripts/SetupSQLite.sh
@@ -0,0 +1,19 @@
+#/bin/bash
+
+rm -f ../../data/sqlite/CampaignFin14.db
+
+rm -f ./CRP_Categories.txt
+rm -f ./*.sanitized
+
+cp ../../data/raw/CampaignFin14/CRP_Categories.txt ./
+cat ../../data/raw/CampaignFin14/cands14.txt | sed -e 's/"//g' -e 's/|/"/g' | ../SanitizeCSV.py  > ./cands14.sanitized
+cat ../../data/raw/CampaignFin14/cmtes14.txt | sed -e 's/|/"/g' | ../SanitizeCSV.py > ./cmtes14.sanitized
+cat ../../data/raw/CampaignFin14/pacs14.txt | sed -e 's/|/"/g' | ../SanitizeCSV.py  > ./pacs14.sanitized
+cat ../../data/raw/CampaignFin14/pac_other14.txt | sed -e 's/|/"/g' | ../SanitizeCSV.py > ./pac_other14.sanitized
+cat ../../data/raw/CampaignFin14/indivs14.txt | sed -e 's/|/"/g' | ../SanitizeCSV.py > ./indivs14.sanitized
+
+sqlite3 ../../data/sqlite/CampaignFin14.db < ../CreateTables.sql
+sqlite3 ../../data/sqlite/CampaignFin14.db < ./ImportCSV.sql
+
+rm -f ./CRP_Categories.txt
+rm -f ./*.sanitized
