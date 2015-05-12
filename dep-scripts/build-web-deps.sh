7fcd699ea7742d0b6e109e188a157d37804c60a6
Alex Chovanec <alex.chovanec@gmail.com>
2015-05-12 00:44:30 -0700

    start using the Node.js graphlib library with browserify to represent the graph data structure

--- dep-scripts/build-web-deps.sh
+++ dep-scripts/build-web-deps.sh
@@ -2,4 +2,4 @@
 
 browserify -r url -s Url -o ../web-content/url-bundle.js
 browserify -r underscore -s _ -o ../web-content/underscore-bundle.js
-
+browserify -r graphlib -s Graph -o ../web-content/graphlib-bundle.js
