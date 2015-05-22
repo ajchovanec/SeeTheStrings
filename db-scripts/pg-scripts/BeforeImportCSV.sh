9155ca39c8af9d4767682867d4e6453117788854
Alex Chovanec <alex.chovanec@gmail.com>
2015-05-22 00:34:42 -0700

    Remove SQLite import scripts

--- db-scripts/pg-scripts/BeforeImportCSV.sh
+++ db-scripts/pg-scripts/BeforeImportCSV.sh
@@ -3,9 +3,9 @@
 rm -f ./CRP_Categories.txt
 rm -f ./*.sanitized
 
-cp ../../data/raw/CampaignFin14/CRP_Categories.txt ./
-cat ../../data/raw/CampaignFin14/cands14.txt | sed -e 's/"//g' -e 's/|/"/g' | ../SanitizeCSV.py  > ./cands14.sanitized
-cat ../../data/raw/CampaignFin14/cmtes14.txt | sed -e 's/|/"/g' | ../SanitizeCSV.py > ./cmtes14.sanitized
-cat ../../data/raw/CampaignFin14/pacs14.txt | sed -e 's/|/"/g' | ../SanitizeCSV.py  > ./pacs14.sanitized
-cat ../../data/raw/CampaignFin14/pac_other14.txt | sed -e 's/|/"/g' | ../SanitizeCSV.py > ./pac_other14.sanitized
-cat ../../data/raw/CampaignFin14/indivs14.txt | sed -e 's/\\|/|/g' -e 's/|/"/g' | ../SanitizeCSV.py > ./indivs14.sanitized
+cp ../../data/CampaignFin14/CRP_Categories.txt ./
+cat ../../data/CampaignFin14/cands14.txt | sed -e 's/"//g' -e 's/|/"/g' | ../SanitizeCSV.py  > ./cands14.sanitized
+cat ../../data/CampaignFin14/cmtes14.txt | sed -e 's/|/"/g' | ../SanitizeCSV.py > ./cmtes14.sanitized
+cat ../../data/CampaignFin14/pacs14.txt | sed -e 's/|/"/g' | ../SanitizeCSV.py  > ./pacs14.sanitized
+cat ../../data/CampaignFin14/pac_other14.txt | sed -e 's/|/"/g' | ../SanitizeCSV.py > ./pac_other14.sanitized
+cat ../../data/CampaignFin14/indivs14.txt | sed -e 's/\\|/|/g' -e 's/|/"/g' | ../SanitizeCSV.py > ./indivs14.sanitized
--- db-scripts/sqlite-scripts/ImportCSV.sql
+++
@@ -1,9 +0,0 @@
-.separator "\t"
-.import CRP_Categories.txt Categories
-
-.separator |
-.import cands14.sanitized Candidates
-.import cmtes14.sanitized Committees
-.import pacs14.sanitized PACsToCandidates
-.import pac_other14.sanitized PACsToPACs
-.import indivs14.sanitized IndivsToAny
